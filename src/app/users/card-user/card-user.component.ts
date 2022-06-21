b215d8b6c9ae29331589bee498da1b98a626d739
HL112 <forest.tl112@gmail.com>
2022-06-22 00:24:47 +0700

    feat: Card user

---
+++ src/app/users/card-user/card-user.component.ts
@@ -0,0 +1,19 @@
+import {Component, Input, OnInit} from '@angular/core';
+import {UserListComponent} from "../user-list/user-list.component";
+import {User} from "../shared/user";
+
+@Component({
+  selector: 'app-card-user',
+  templateUrl: './card-user.component.html',
+  styleUrls: ['./card-user.component.css']
+})
+export class CardUserComponent implements OnInit {
+
+  @Input() user? : User;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
